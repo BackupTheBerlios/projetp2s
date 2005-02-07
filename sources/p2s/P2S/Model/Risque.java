8534fbec3140874e8da363e99b78acb4097c66c2
smash31 <smash31>
2005-02-07 23:08:07 +0000

    no message

---
+++ sources/p2s/P2S/Model/Risque.java
@@ -0,0 +1,69 @@
+/*
+ * Risque.java
+ *
+ * Created on 7 février 2005, 23:59
+ */
+
+package P2S.Model;
+
+/**
+ *
+ * @author Fabien
+ */
+public class Risque {
+    
+    private String nom;
+    private String description;
+    private int priorite;
+    private int impact;
+    private int etat;
+    
+    /** Creates a new instance of Risque */
+    public Risque(String _nom, String _description, int _priorite, int _impact, int _etat) {
+        this.setNom(_nom);
+        this.setDescription(_description);
+        this.setPriorite(_priorite);
+        this.setImpact(_impact);
+        this.setEtat(_etat);
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getPriorite() {
+        return priorite;
+    }
+
+    public void setPriorite(int priorite) {
+        this.priorite = priorite;
+    }
+
+    public int getImpact() {
+        return impact;
+    }
+
+    public void setImpact(int impact) {
+        this.impact = impact;
+    }
+
+    public int getEtat() {
+        return etat;
+    }
+
+    public void setEtat(int etat) {
+        this.etat = etat;
+    }    
+}
