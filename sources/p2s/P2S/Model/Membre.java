4b1ad260a00701d1fd21f1d8b829d87d917f3129
cox65 <cox65>
2005-02-08 00:22:53 +0000

    no message

--- sources/p2s/P2S/Model/Membre.java
+++ sources/p2s/P2S/Model/Membre.java
@@ -16,96 +16,110 @@ public class Membre {
     
     //ATTRIBUTS
     
-    private int idMembre;
     private String nom;
-    private String prenom;    
+    private String prenom;
     private String adresse;
     private String telephone;
     private String email;
+    private Vector listeRoles;
     private Vector listeTaches;
     private Vector listeArtefacts;
     
     //CONSTRUCTEURS
     
+    //Constructeur sans parametre
     public Membre() {}
-
-    public Membre(int _idMembre,String _nom,String _prenom,String _adresse,String _telephone,String _email,Vector _listeTaches,Vector _listeArtefacts) {
-        idMembre = _idMembre;
-        nom = _nom;
-        prenom = _prenom;        
-        adresse = _adresse;
-        telephone = _telephone;
-        email = _email;
-        setListeTaches(_listeTaches);
-        listeArtefacts = _listeArtefacts;
+    
+    //Constructeur sans liste
+    public Membre(String _nom,String _prenom,String _adresse,String _telephone,String _email) {
+        this.nom = _nom;
+        this.prenom = _prenom;
+        this.adresse = _adresse;
+        this.telephone = _telephone;
+        this.email = _email;
+        this.listeRoles = new Vector();
+        this.listeTaches = new Vector();
+        this.listeArtefacts = new Vector();
+    }
+    
+    //Constructeur complet
+    public Membre(String _nom,String _prenom,String _adresse,String _telephone,String _email,Vector _listeRoles,Vector _listeTaches,Vector _listeArtefacts) {
+        this.nom = _nom;
+        this.prenom = _prenom;
+        this.adresse = _adresse;
+        this.telephone = _telephone;
+        this.email = _email;
+        this.setListeRoles(_listeRoles);
+        this.listeTaches = listeTaches;
+        this.listeArtefacts = _listeArtefacts;
     }
     
     
     //GETTEURS ET SETTEURS
     
-    public int getIdMembre() {
-        return idMembre;
-    }
-
-    public void setIdMembre(int idMembre) {
-        this.idMembre = idMembre;
-    }
-
     public String getNom() {
         return nom;
     }
-
+    
     public void setNom(String nom) {
         this.nom = nom;
     }
-
+    
     public String getPrenom() {
         return prenom;
     }
-
+    
     public void setPrenom(String prenom) {
         this.prenom = prenom;
     }
-
+    
     
     public String getAdresse() {
         return adresse;
     }
-
+    
     public void setAdresse(String adresse) {
         this.adresse = adresse;
     }
-
+    
     public String getTelephone() {
         return telephone;
     }
-
+    
     public void setTelephone(String telephone) {
         this.telephone = telephone;
     }
-
+    
     public String getEmail() {
         return email;
     }
-
+    
     public void setEmail(String email) {
         this.email = email;
     }
-
+    
     public Vector getListeTaches() {
         return listeTaches;
     }
-
+    
     public void setListeTaches(Vector listeTaches) {
         this.listeTaches = listeTaches;
     }
-
+    
     public Vector getListeArtefacts() {
         return listeArtefacts;
     }
-
+    
     public void setListeArtefacts(Vector listeArtefacts) {
         this.listeArtefacts = listeArtefacts;
+    }
+    
+    public Vector getListeRoles() {
+        return listeRoles;
+    }
+    
+    public void setListeRoles(Vector listeRoles) {
+        this.listeRoles = listeRoles;
     }
     
 }
