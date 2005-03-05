a530334cb7901a246d876a683ea73eb594311bb6
l3isi37 <l3isi37>
2005-03-05 14:09:36 +0000

    creation

---
+++ sources/p2s/P2S/UI/Graphic2D/GrapheIndicateursProjet.java
@@ -0,0 +1,22 @@
+/*
+ * GrapheIndicateursProjet.java
+ *
+ * Created on 5 mars 2005, 15:11
+ */
+
+package P2S.UI.Graphic2D;
+
+import java.awt.Graphics2D ;
+
+
+/**
+ *
+ * @author Conde Mike K
+ */
+public class GrapheIndicateursProjet {
+    
+    /** Creates a new instance of GrapheIndicateursProjet */
+    public GrapheIndicateursProjet () {
+    }
+    
+}
