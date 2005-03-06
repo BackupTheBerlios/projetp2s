a9f2d4c5ff9011b07c26c07fee9ecf128c8de260
l3isi37 <l3isi37>
2005-03-06 23:41:34 +0000

    no message

--- sources/p2s/P2S/UI/View/JDialog/JDialogLogin.java
+++ sources/p2s/P2S/UI/View/JDialog/JDialogLogin.java
@@ -198,9 +198,9 @@ public class JDialogLogin extends javax.swing.JDialog {
                     ((JFrameP2S)this.getParent()).utilisateur = new Superviseur(this.JTextFieldLogin.getText(), Md5.getEncodedPassword(this.JPasswordFieldMDP.getText()), parser.lireProjets());
                 } else if(parser.lireFonction().compareTo("dir") == 0) {
                     ((JFrameP2S)this.getParent()).utilisateur = new Directeur(this.JTextFieldLogin.getText(),parser.lireMembres());
-                }/*else if(parser.lireFonction().compareTo("dir") == 0) {
-                    ((JFrameP2S)this.getParent()).utilisateur = new ChefDeProjet(this.JTextFieldLogin.getText(),null);
-                }*/
+                }else if(parser.lireFonction().compareTo("cdp") == 0) {
+                    ((JFrameP2S)this.getParent()).utilisateur = new ChefDeProjet(this.JTextFieldLogin.getText(),Md5.getEncodedPassword(this.JPasswordFieldMDP.getText()), parser.lireProjets());
+                }
             }
             in.close();
         } catch(MalformedURLException e1){
