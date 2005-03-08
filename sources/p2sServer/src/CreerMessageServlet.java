486e286951a6e8601e29861c3b54d7088d40eaac
l3isi32 <l3isi32>
2005-03-08 08:44:33 +0000

    no message

---
+++ sources/p2sServer/src/CreerMessageServlet.java
@@ -0,0 +1,108 @@
+/*
+ * CreerMessageServlet.java
+ *
+ * Created on 7 mars 2005, 00:43
+ */
+
+import java.io.*;
+import java.net.*;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.DriverManager;
+import java.sql.Statement;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+/**
+ *
+ * @author Kassem
+ * @version
+ */
+public class CreerMessageServlet extends HttpServlet {
+    
+    /** Initializes the servlet.
+     */
+
+    public void init(ServletConfig config) throws ServletException {
+        super.init(config);
+        
+        try {
+            Class.forName("com.mysql.jdbc.Driver").newInstance();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        
+    }
+    
+    /** Destroys the servlet.
+     */
+    public void destroy() {
+        
+    }
+    
+     /** Processes requests for both HTTP <code>GET</code> and <code>POST</code> methods.
+     * @param request servlet request
+     * @param response servlet response
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        
+        String login = request.getParameter("login");
+        String sujet = request.getParameter("sujet");
+        //String message = request.getParameter("message");
+        
+        out.println("dans");
+        //out.println("nit" + login);
+        
+        //if(login != null  && sujet != null){
+             try {
+                ParserConnexionBD parser = new ParserConnexionBD(getServletContext().getRealPath("/ConnexionBD.xml"));
+                // Connexion a la base de donnees                                
+                Connection conn = DriverManager.getConnection("jdbc:mysql://"+parser.lireHost()+"/"+parser.lireBase()+"?user="+parser.lireLogin()+"&password="+parser.lirePassword());
+              
+                out.println("ok");
+                Statement s = conn.createStatement();
+                System.out.println("avant req");
+                s.executeUpdate("Insert into messages (login,sujet) values('" + login + "','" + sujet + "')");
+                System.out.println("aprés req");
+                s.close();
+                conn.close();
+             }
+             catch(SQLException e){
+                 e.printStackTrace();
+
+             //}
+        }   
+        out.close(); 
+    }
+    
+    /** Handles the HTTP <code>GET</code> method.
+     * @param request servlet request
+     * @param response servlet response
+     */
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+        processRequest(request, response);
+    }
+    
+    /** Handles the HTTP <code>POST</code> method.
+     * @param request servlet request
+     * @param response servlet response
+     */
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+        processRequest(request, response);
+    }
+    
+    /** Returns a short description of the servlet.
+     */
+    public String getServletInfo() {
+        return "Short description";
+    }
+    
+}
