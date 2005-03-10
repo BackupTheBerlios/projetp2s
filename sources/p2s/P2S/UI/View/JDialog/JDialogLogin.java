88cf76115be31334e2cc7f2b42c4de571f5ab615
l3isi32 <l3isi32>
2005-03-10 19:56:34 +0000

    no message

--- sources/p2s/P2S/UI/View/JDialog/JDialogLogin.java
+++ sources/p2s/P2S/UI/View/JDialog/JDialogLogin.java
@@ -154,11 +154,11 @@ public class JDialogLogin extends javax.swing.JDialog {
                 ParserXMLLog parser = new ParserXMLLog(fluxXml);
                                 
                 if(parser.lireFonction().compareTo("sup") == 0) {
-                    ((JFrameP2S)this.getParent()).utilisateur = new Superviseur(this.JTextFieldLogin.getText(), Md5.getEncodedPassword(this.JPasswordFieldMDP.getText()), parser.lireProjets());
+                    ((JFrameP2S)this.getParent()).utilisateur = new Superviseur(this.JTextFieldLogin.getText(), Md5.getEncodedPassword(this.JPasswordFieldMDP.getText()), parser.lireProjets(), parser.lireMessages());
                 } else if(parser.lireFonction().compareTo("dir") == 0) {
                     ((JFrameP2S)this.getParent()).utilisateur = new Directeur(this.JTextFieldLogin.getText(),parser.lireMembres());
                 }else if(parser.lireFonction().compareTo("cdp") == 0) {
-                    ((JFrameP2S)this.getParent()).utilisateur = new ChefDeProjet(this.JTextFieldLogin.getText(),Md5.getEncodedPassword(this.JPasswordFieldMDP.getText()), parser.lireProjets());
+                    ((JFrameP2S)this.getParent()).utilisateur = new ChefDeProjet(this.JTextFieldLogin.getText(),Md5.getEncodedPassword(this.JPasswordFieldMDP.getText()), parser.lireProjets(), parser.lireMessages());
                 }
             }
             in.close();
---
+++ sources/p2sServer/src/SupprimerUnMessageServlet.java
@@ -0,0 +1,95 @@
+/*
+ * SupprimerUnMessageServlet.java
+ *
+ * Created on 10 mars 2005, 20:48
+ */
+
+import java.io.*;
+import java.net.*;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.DriverManager;
+import java.sql.Statement;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+/**
+ *
+ * @author kass
+ * @version
+ */
+public class SupprimerUnMessageServlet extends HttpServlet {
+    
+    /** Initializes the servlet.
+     */
+    public void init(ServletConfig config) throws ServletException {
+        super.init(config);
+        
+    }
+    
+    /** Destroys the servlet.
+     */
+    public void destroy() {
+        
+    }
+    
+    /** Processes requests for both HTTP <code>GET</code> and <code>POST</code> methods.
+     * @param request servlet request
+     * @param response servlet response
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+       
+        String sujet = request.getParameter("sujet");
+        String message = request.getParameter("message");
+       
+       
+        try {
+                ParserConnexionBD parser = new ParserConnexionBD(getServletContext().getRealPath("/ConnexionBD.xml"));
+                // Connexion a la base de donnees                                
+                Connection conn = DriverManager.getConnection("jdbc:mysql://"+parser.lireHost()+"/"+parser.lireBase()+"?user="+parser.lireLogin()+"&password="+parser.lirePassword());
+        
+                Statement s = conn.createStatement();
+                s.executeUpdate("delete from messages where sujet = '" + sujet + "' and message= '" + message + "')");
+                out.println("ok");
+                s.close();
+                    
+             }
+               
+             catch(SQLException e){
+                 e.printStackTrace();
+             }
+        
+        out.close();
+    }
+    
+    /** Handles the HTTP <code>GET</code> method.
+     * @param request servlet request
+     * @param response servlet response
+     */
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+        processRequest(request, response);
+    }
+    
+    /** Handles the HTTP <code>POST</code> method.
+     * @param request servlet request
+     * @param response servlet response
+     */
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+        processRequest(request, response);
+    }
+    
+    /** Returns a short description of the servlet.
+     */
+    public String getServletInfo() {
+        return "Short description";
+    }
+    
+}
