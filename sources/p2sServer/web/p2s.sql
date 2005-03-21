95b75f529e7f7afa24fe6123d78e9d7c98dd832d
cox65 <cox65>
2005-03-21 20:57:24 +0000

    no message

--- sources/p2sServer/web/p2s.sql
+++ sources/p2sServer/web/p2s.sql
@@ -10,6 +10,8 @@ SET NAMES utf8;
 
 CREATE DATABASE /*!32312 IF NOT EXISTS*/ `p2s`;
 USE `p2s`;
+
+DROP TABLE IF EXISTS `artefacts`;
 CREATE TABLE `artefacts` (
   `idartefact` int(10) unsigned NOT NULL auto_increment,
   `livrable` enum('true','false') default NULL,
@@ -21,6 +23,8 @@ CREATE TABLE `artefacts` (
   KEY `FK_artefacts_idresponsable` (`idresponsable`),
   CONSTRAINT `FK_artefacts_idresponsable` FOREIGN KEY (`idresponsable`) REFERENCES `membres` (`idmembre`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+DROP TABLE IF EXISTS `artefacts_entrees_taches`;
 CREATE TABLE `artefacts_entrees_taches` (
   `idartefact` int(10) unsigned NOT NULL auto_increment,
   `idtache` int(10) unsigned NOT NULL default '0',
@@ -29,6 +33,8 @@ CREATE TABLE `artefacts_entrees_taches` (
   CONSTRAINT `FK_artefacts_entrees_taches_idartefact` FOREIGN KEY (`idartefact`) REFERENCES `artefacts` (`idartefact`),
   CONSTRAINT `FK_artefacts_entrees_taches_idtache` FOREIGN KEY (`idtache`) REFERENCES `taches` (`idtache`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+DROP TABLE IF EXISTS `artefacts_entrees_tachescollaboratives`;
 CREATE TABLE `artefacts_entrees_tachescollaboratives` (
   `idartefact` int(10) unsigned NOT NULL auto_increment,
   `idtache` int(10) unsigned NOT NULL default '0',
@@ -37,6 +43,8 @@ CREATE TABLE `artefacts_entrees_tachescollaboratives` (
   CONSTRAINT `FK_artefacts_entrees_tachescollaboratives_idartefact` FOREIGN KEY (`idartefact`) REFERENCES `artefacts` (`idartefact`),
   CONSTRAINT `FK_artefacts_entrees_tachescollaboratives_idtache` FOREIGN KEY (`idtache`) REFERENCES `tachescollaboratives` (`idtache`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COMMENT='InnoDB free: 3072 kB';
+
+DROP TABLE IF EXISTS `artefacts_sorties_taches`;
 CREATE TABLE `artefacts_sorties_taches` (
   `idartefact` int(10) unsigned NOT NULL auto_increment,
   `idtache` int(10) unsigned NOT NULL default '0',
@@ -45,6 +53,8 @@ CREATE TABLE `artefacts_sorties_taches` (
   CONSTRAINT `FK_artefacts_sorties_taches_idartefact` FOREIGN KEY (`idartefact`) REFERENCES `artefacts` (`idartefact`),
   CONSTRAINT `FK_artefacts_sorties_taches_idtache` FOREIGN KEY (`idtache`) REFERENCES `taches` (`idtache`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+DROP TABLE IF EXISTS `artefacts_sorties_tachescollaboratives`;
 CREATE TABLE `artefacts_sorties_tachescollaboratives` (
   `idartefact` int(10) unsigned NOT NULL auto_increment,
   `idtache` int(10) unsigned NOT NULL default '0',
@@ -53,6 +63,8 @@ CREATE TABLE `artefacts_sorties_tachescollaboratives` (
   CONSTRAINT `FK_artefacts_sorties_tachescollaboratives_idartefact` FOREIGN KEY (`idartefact`) REFERENCES `artefacts` (`idartefact`),
   CONSTRAINT `FK_artefacts_sorties_tachescollaboratives_idtache` FOREIGN KEY (`idtache`) REFERENCES `tachescollaboratives` (`idtache`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+DROP TABLE IF EXISTS `chefprojet_projets`;
 CREATE TABLE `chefprojet_projets` (
   `login` varchar(45) NOT NULL default '',
   `idprojet` int(10) unsigned NOT NULL default '0',
@@ -61,6 +73,8 @@ CREATE TABLE `chefprojet_projets` (
   CONSTRAINT `FK_chefprojet_projets_idprojet` FOREIGN KEY (`idprojet`) REFERENCES `projets` (`idprojet`),
   CONSTRAINT `FK_chefprojet_projets_login` FOREIGN KEY (`login`) REFERENCES `utilisateurs` (`login`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+DROP TABLE IF EXISTS `indicateurs_iteration`;
 CREATE TABLE `indicateurs_iteration` (
   `iditeration` int(10) unsigned NOT NULL auto_increment,
   `totalcharges` int(10) unsigned default NULL,
@@ -72,6 +86,8 @@ CREATE TABLE `indicateurs_iteration` (
   PRIMARY KEY  (`iditeration`),
   CONSTRAINT `FK_indicateurs_iteration_iditeration` FOREIGN KEY (`iditeration`) REFERENCES `iterations` (`iditeration`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+DROP TABLE IF EXISTS `indicateurs_projet`;
 CREATE TABLE `indicateurs_projet` (
   `idprojet` int(10) unsigned NOT NULL auto_increment,
   `totalcharges` int(10) unsigned default NULL,
@@ -82,6 +98,8 @@ CREATE TABLE `indicateurs_projet` (
   PRIMARY KEY  (`idprojet`),
   CONSTRAINT `FK_indicateurs_projet_idprojet` FOREIGN KEY (`idprojet`) REFERENCES `projets` (`idprojet`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+DROP TABLE IF EXISTS `iteration_mesures`;
 CREATE TABLE `iteration_mesures` (
   `iditeration` int(10) unsigned NOT NULL default '0',
   `idmesure` int(10) unsigned NOT NULL default '0',
@@ -90,6 +108,8 @@ CREATE TABLE `iteration_mesures` (
   CONSTRAINT `FK_iteration_mesures_iditeration` FOREIGN KEY (`iditeration`) REFERENCES `iterations` (`iditeration`),
   CONSTRAINT `FK_iteration_mesures_idmesure` FOREIGN KEY (`idmesure`) REFERENCES `mesures` (`idmesure`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+DROP TABLE IF EXISTS `iterations`;
 CREATE TABLE `iterations` (
   `iditeration` int(10) unsigned NOT NULL auto_increment,
   `numero` int(10) unsigned NOT NULL default '0',
@@ -102,6 +122,8 @@ CREATE TABLE `iterations` (
   KEY `FK_iterations_idprojet` (`idprojet`),
   CONSTRAINT `FK_iterations_idprojet` FOREIGN KEY (`idprojet`) REFERENCES `projets` (`idprojet`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COMMENT='InnoDB free: 4096 kB';
+
+DROP TABLE IF EXISTS `membres`;
 CREATE TABLE `membres` (
   `idmembre` int(10) unsigned NOT NULL default '0',
   `nom` varchar(45) NOT NULL default '',
@@ -111,6 +133,8 @@ CREATE TABLE `membres` (
   `email` varchar(45) default NULL,
   PRIMARY KEY  (`idmembre`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+DROP TABLE IF EXISTS `membres_projets`;
 CREATE TABLE `membres_projets` (
   `idmembre` int(10) unsigned NOT NULL auto_increment,
   `idprojet` int(10) unsigned NOT NULL default '0',
@@ -119,6 +143,8 @@ CREATE TABLE `membres_projets` (
   CONSTRAINT `FK_membres_projets_idmembre` FOREIGN KEY (`idmembre`) REFERENCES `membres` (`idmembre`),
   CONSTRAINT `FK_membres_projets_idprojet` FOREIGN KEY (`idprojet`) REFERENCES `projets` (`idprojet`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+DROP TABLE IF EXISTS `membres_tachescollaboratives`;
 CREATE TABLE `membres_tachescollaboratives` (
   `idmembre` int(10) unsigned NOT NULL auto_increment,
   `idtache` int(10) unsigned NOT NULL default '0',
@@ -127,6 +153,8 @@ CREATE TABLE `membres_tachescollaboratives` (
   CONSTRAINT `FK_membrestachescolaboratives_idmembre` FOREIGN KEY (`idmembre`) REFERENCES `membres` (`idmembre`),
   CONSTRAINT `FK_membrestachescolaboratives_idtache` FOREIGN KEY (`idtache`) REFERENCES `tachescollaboratives` (`idtache`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+DROP TABLE IF EXISTS `messages`;
 CREATE TABLE `messages` (
   `idmessage` int(10) unsigned NOT NULL auto_increment,
   `login` varchar(45) NOT NULL default '',
@@ -135,6 +163,8 @@ CREATE TABLE `messages` (
   `message` tinytext,
   PRIMARY KEY  (`idmessage`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+DROP TABLE IF EXISTS `mesures`;
 CREATE TABLE `mesures` (
   `idmesure` int(10) unsigned NOT NULL auto_increment,
   `nom` varchar(45) NOT NULL default '',
@@ -147,6 +177,8 @@ CREATE TABLE `mesures` (
   KEY `FK_mesures_idmembre` (`idmembre`),
   CONSTRAINT `FK_mesures_idmembre` FOREIGN KEY (`idmembre`) REFERENCES `membres` (`idmembre`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+DROP TABLE IF EXISTS `problemes`;
 CREATE TABLE `problemes` (
   `idprobleme` int(10) unsigned NOT NULL auto_increment,
   `nom` varchar(45) NOT NULL default '',
@@ -158,6 +190,8 @@ CREATE TABLE `problemes` (
   KEY `FK_probemes_idprojet` (`idprojet`),
   CONSTRAINT `FK_problemes_idprojet` FOREIGN KEY (`idprojet`) REFERENCES `projets` (`idprojet`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+DROP TABLE IF EXISTS `projets`;
 CREATE TABLE `projets` (
   `idprojet` int(10) unsigned NOT NULL auto_increment,
   `nom` varchar(45) NOT NULL default '',
@@ -169,6 +203,8 @@ CREATE TABLE `projets` (
   `fichier` varchar(150) NOT NULL default '',
   PRIMARY KEY  (`idprojet`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+DROP TABLE IF EXISTS `risques`;
 CREATE TABLE `risques` (
   `idrisque` int(10) unsigned NOT NULL auto_increment,
   `nom` varchar(45) NOT NULL default '',
@@ -181,12 +217,16 @@ CREATE TABLE `risques` (
   KEY `FK_risques_idprojet` (`idprojet`),
   CONSTRAINT `FK_risques_idprojet` FOREIGN KEY (`idprojet`) REFERENCES `projets` (`idprojet`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+DROP TABLE IF EXISTS `roles`;
 CREATE TABLE `roles` (
   `idrole` varchar(45) NOT NULL default '',
   `nom` varchar(45) NOT NULL default '',
   `description` varchar(200) default NULL,
   PRIMARY KEY  (`idrole`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+DROP TABLE IF EXISTS `roles_membres`;
 CREATE TABLE `roles_membres` (
   `idrole` varchar(45) NOT NULL default '',
   `idmembre` int(10) unsigned NOT NULL default '0',
@@ -195,6 +235,8 @@ CREATE TABLE `roles_membres` (
   CONSTRAINT `FK_roles_membres_idmembre` FOREIGN KEY (`idmembre`) REFERENCES `membres` (`idmembre`),
   CONSTRAINT `FK_roles_membres_idrole` FOREIGN KEY (`idrole`) REFERENCES `roles` (`idrole`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+DROP TABLE IF EXISTS `seuilsfixes_projet`;
 CREATE TABLE `seuilsfixes_projet` (
   `idprojet` int(10) unsigned NOT NULL default '0',
   `totalChargesProjetMin` int(10) unsigned default '0',
@@ -222,6 +264,8 @@ CREATE TABLE `seuilsfixes_projet` (
   PRIMARY KEY  (`idprojet`),
   CONSTRAINT `FK_seuilsfixes_projet_1` FOREIGN KEY (`idprojet`) REFERENCES `projets` (`idprojet`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COMMENT='InnoDB free: 4096 kB';
+
+DROP TABLE IF EXISTS `superviseur_projets`;
 CREATE TABLE `superviseur_projets` (
   `login` varchar(45) NOT NULL default '',
   `idprojet` int(10) unsigned NOT NULL default '0',
@@ -230,6 +274,8 @@ CREATE TABLE `superviseur_projets` (
   CONSTRAINT `FK_superviseurprojets_idprojet` FOREIGN KEY (`idprojet`) REFERENCES `projets` (`idprojet`),
   CONSTRAINT `FK_superviseurprojets_login` FOREIGN KEY (`login`) REFERENCES `utilisateurs` (`login`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COMMENT='InnoDB free: 4096 kB';
+
+DROP TABLE IF EXISTS `taches`;
 CREATE TABLE `taches` (
   `idtache` int(10) unsigned NOT NULL auto_increment,
   `nom` varchar(45) NOT NULL default '',
@@ -250,6 +296,8 @@ CREATE TABLE `taches` (
   CONSTRAINT `FK_taches_iditeration` FOREIGN KEY (`iditeration`) REFERENCES `iterations` (`iditeration`),
   CONSTRAINT `FK_taches_idmembre` FOREIGN KEY (`idmembre`) REFERENCES `membres` (`idmembre`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+DROP TABLE IF EXISTS `tachescollaboratives`;
 CREATE TABLE `tachescollaboratives` (
   `idtache` int(10) unsigned NOT NULL default '0',
   `nom` varchar(45) NOT NULL default '',
@@ -270,6 +318,8 @@ CREATE TABLE `tachescollaboratives` (
   CONSTRAINT `FK_tachescolaboratives_iditeration` FOREIGN KEY (`iditeration`) REFERENCES `iterations` (`iditeration`),
   CONSTRAINT `FK_tachescollaboratives_idresponsable` FOREIGN KEY (`idresponsable`) REFERENCES `membres` (`idmembre`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COMMENT='InnoDB free: 3072 kB';
+
+DROP TABLE IF EXISTS `utilisateurs`;
 CREATE TABLE `utilisateurs` (
   `login` varchar(45) NOT NULL default '',
   `password` varchar(45) NOT NULL default '',
