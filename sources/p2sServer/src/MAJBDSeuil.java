08cef41d24ba3cee18c3949b19166f9ed2f0b3e3
escuzze <escuzze>
2005-03-21 23:19:40 +0000

    no message

---
+++ sources/p2sServer/src/MAJBDSeuil.java
@@ -0,0 +1,70 @@
+/*
+ * MAJBDSeuil.java
+ *
+ * Created on 21 mars 2005, 23:56
+ */
+
+import java.io.*;
+import java.net.*;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+/**
+ *
+ * @author Guillaume
+ * @version
+ */
+public class MAJBDSeuil extends HttpServlet {
+    
+    /** Initializes the servlet.
+     */
+    public void init(ServletConfig config) throws ServletException {
+        super.init(config);
+    }
+    
+    /** Destroys the servlet.
+     */
+    public void destroy() {
+        
+    }
+    
+    /** Processes requests for both HTTP <code>GET</code> and <code>POST</code> methods.
+     * @param request servlet request
+     * @param response servlet response
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+        
+        String login = request.getParameter("login");
+        String lecture = request.getParameter("lecture");
+        String nomChamp = request.getParameter("nomChamp");
+        String valeur = request.getParameter("value");
+        
+    }
+    
+    /** Handles the HTTP <code>GET</code> method.
+     * @param request servlet request
+     * @param response servlet response
+     */
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+        processRequest(request, response);
+    }
+    
+    /** Handles the HTTP <code>POST</code> method.
+     * @param request servlet request
+     * @param response servlet response
+     */
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+        processRequest(request, response);
+    }
+    
+    /** Returns a short description of the servlet.
+     */
+    public String getServletInfo() {
+        return "Short description";
+    }
+    
+}
